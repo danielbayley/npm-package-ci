d94ea69cbbd8db3a094a11d703261b351c0831d0
Daniel Bayley <daniel.bayley@me.com>
2024-04-24 14:39:00 +0100

    feat: add custom package manager detection script

---
+++ index.js
@@ -0,0 +1,51 @@
+import { readdir } from "node:fs/promises"
+import { read } from "utils"
+
+let { INPUT_REPORTER: reporter } = process.env
+const { dirname } = import.meta
+const { dependencies } = await read(`${dirname}/package.json`).then(JSON.parse)
+const [name] = Object.keys(dependencies)
+const packageJSON = "package.json"
+const files = {
+  [packageJSON]:      undefined,
+  "package.yaml":        "pnpm",
+  "pnpm-lock.yaml":      "pnpm",
+  "shrinkwrap.yaml":     "pnpm",
+  "yarn.lock":           "yarn",
+  "package-lock.json":    "npm",
+  "npm-shrinkwrap.json":  "npm",
+}
+const ls = await readdir(".")
+const path = ls.find(path => files[path])
+const content = await read(packageJSON).catch(error => "{}")
+const { packageManager, engines } = JSON.parse(content)
+
+let [pm] = packageManager?.split("@") ?? Object
+  .values(files)
+  .filter(pm => engines?.[pm])
+
+pm ??= files[path] ?? "npm"
+console.log(`package-manager=${pm}`)
+
+const install = {
+  pnpm: "pnpm install",
+  yarn: "yarn install --immutable",
+  npm:  "npm ci",
+}
+console.log(`install=${install[pm]}`)
+
+const test = pm === "yarn" ? "yarn test" : `${pm} run --if-present test`
+console.log(`test=${test}`)
+
+if (reporter !== "false") {
+  reporter ||= `${dirname}/packages/${name}/index.js`
+  const add = {
+    pnpm: `pnpm add ${reporter}`,
+    yarn: `yarn add ${reporter}`,
+    npm:  `npm  add ${reporter} --no-save`,
+  }
+  console.log(`add=${add[pm]}`)
+
+  const options = `--test-reporter=${reporter} --test-reporter-destination=$GITHUB_STEP_SUMMARY`
+  console.log(`options=${options}`)
+}
