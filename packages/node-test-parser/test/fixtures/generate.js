be8217363d373a2d490613cf6efb52d70f157f96
Daniel Bayley <daniel.bayley@me.com>
2025-02-08 20:25:32 +0000

    fix: test parser error handler

--- packages/node-test-parser/test/fixtures/generate.js
+++ packages/node-test-parser/test/fixtures/generate.js
@@ -1,6 +1,6 @@
 import { arrayFrom } from "utils"
 
 export default async function* generate(report) {
-  const events = await arrayFrom(report)
+  const events = await arrayFrom(report).catch(() => ({}))
   yield JSON.stringify(events, null, 2)
 }
